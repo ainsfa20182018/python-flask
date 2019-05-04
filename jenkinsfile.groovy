ef77ddf7495eec85af4f0b66331adb61b986530a
ainsfa20182018 <ainsfa20182018@gmail.com>
2019-05-04 15:19:37 -0500

    add

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,12 +2,12 @@ node{
     properties([parameters([string(defaultValue: 'IP', description: 'where to build  IP', name: 'Env', trim: false)])])
     stage("Clone repo"){
         git 'https://github.com/ainsfa20182018/python-flask.git'
-    }
+     }
     stage("Build application"){
         sh "scp -r * ec2-user@${ENV}:/tmp"
         sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
     }
-     stage("App Run"){
+    stage("App Run"){
         sh "ssh ec2-user@${ENV}  python  /tmp/app.py"
     }
 }
